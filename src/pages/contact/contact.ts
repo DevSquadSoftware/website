291b97cbb8d33763aafd24cc276ba2d509cc1eac
Brandon Seydel <brandonseydel@hotmail.com>
2023-01-20 21:50:38 -0600

    push form changes

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -8,4 +8,16 @@ export class ContactUs implements ICustomElementViewModel {
   constructor() {
     // you can inject the element or any DI in the constructor
   }
+
+  handleSubmit = (event: SubmitEvent) => {
+    event.preventDefault();
+    const myForm = event.target as HTMLFormElement;
+    const formData = [['form-name', 'contact'], ...new FormData(myForm)] as [string, string][];
+
+    void fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    });
+  };
 }
