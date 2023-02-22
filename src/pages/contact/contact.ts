2369290b9a786a382f32ca243f5f2d2420cf74d0
Brandon <brandonseydel@hotmail.com>
2023-02-22 14:36:03 -0500

    push contact changes

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,25 +1,22 @@
 import { customElement, ICustomElementViewModel } from 'aurelia';
 
-import { encode } from '../../commonts';
-
 import './contact.scss';
 
 import template from 'bundle-text:./contact.html';
-import 'url:./contact.html';
+
 @customElement({ name: 'contact', template })
 export class ContactUs implements ICustomElementViewModel {
-  constructor() {
-    // you can inject the element or any DI in the constructor
-  }
-
-  async handleSubmit(event: SubmitEvent) {
+  handleSubmit(event: SubmitEvent) {
     const myForm = event.target as HTMLFormElement;
     const formData = new FormData(myForm) as unknown;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData as [string,string][]).toString(),
+
+    console.log(new URLSearchParams(formData as [string, string][]).toString());
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData as [string, string][]).toString(),
     })
-      .then(() => alert("Thank you for your submission"))
+      .then(() => alert('Thank you for your submission'))
       .catch((error) => alert(error));
+  }
 }
