8f7bd1b14cdda48608f61dc69b4f8498c5ccb280
Brandon <brandonseydel@hotmail.com>
2023-02-22 14:05:39 -0500

    push algolia

--- src/elements/navigation/navigation.ts
+++ src/elements/navigation/navigation.ts
@@ -7,7 +7,7 @@ import template from 'bundle-text:./navigation.html';
 import Navbar from 'mdb-ui-kit/src/mdb/js/pro/navbar';
 @customElement({ name: 'navigation', template })
 export class Navigation implements ICustomElementViewModel {
-  nav: HTMLElement;
+  nav: undefined | HTMLElement;
   constructor(@IRouter private readonly router: IRouter) {
     // you can inject the element or any DI in the constructor
   }
@@ -25,6 +25,8 @@ export class Navigation implements ICustomElementViewModel {
   }
 
   attaching() {
+    if (!this.nav) return;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     new Navbar(this.nav).init();
   }
 }
